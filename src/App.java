f40fc49ecec64eb6acd3c3d0b7628964de24fbdd
lucasgdcosta <lucas_gd_costa@estudante.sesisenai.org.br>
2025-08-22 16:14:38 -0300

    Commit_1
    Primeiro commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
 public class App {
+
+    public static float n1, n2, n3, mf;
+    public static String status;
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        n1 = 5.6f;
+        n2 = 7.5f;
+        n3 = 4.9f;
+
+        mf = (n1+n2+n3)/3;
+
+        if(mf >= 7.0){
+            status = "APROVADO";
+        }
+        else if(mf <5.0){
+            status = "REPROVADO";
+        }
+        else{
+            status = "RECUPERAÇÃO";
+        }
+        System.out.println("STATUS: "+status);
     }
 }
